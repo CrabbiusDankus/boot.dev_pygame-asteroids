de0dd97bdba6ba01f144a7aa47ba1e48d4dba5b3
CrabbyCakes <braden.bierman@gmail.com>
2025-08-25 12:13:44 -0400

    added player (triangle) to screen center

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import pygame 
 from constants import *
+from player import *
 
 def main():
     pygame.init() 
 
-    pygame.time.Clock
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720") 
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     
     BLACK = (0, 0, 0)
 
     running = True
 
     while running:
-     for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-       return
-      screen.fill(BLACK)
-      pygame.display.flip() 
-      pygame.time.Clock.ticket(60)
-      dt = pygame.time.Clock().tick(60) / 1000  # Delta time in seconds.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill(BLACK)
+        player.draw(screen)
+        pygame.display.flip() 
+        
+        
+        dt = clock.tick(60) / 1000  # Delta time in seconds.
+        
 
+    
 if __name__ == "__main__":
     main()
